6b57e1e19d427b42399d2b9c535644b04d994278
AdamenkoVlodymyr <vovaadanenko01@gmail.com>
2019-11-15 13:37:26 +0200

    Убрал логгеры.

--- module1/src/main/java/net/broscorp/d_gc/GarbageCollectionConcept.java
+++ module1/src/main/java/net/broscorp/d_gc/GarbageCollectionConcept.java
@@ -1,31 +1,31 @@
+package net.broscorp.d_gc;
+
 import java.io.IOException;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Handler;
 import java.util.logging.Logger;
 
 public class GarbageCollectionConcept {
 
-  static int count = 1; 
-  private static final Logger LOGGER = Logger.getLogger("myLogger.txt");
-    public static void main(String[] args) throws SecurityException, IOException {
-    
-       Handler logHandler = new ConsoleHandler();
-       LOGGER.addHandler(logHandler);
- 
-           showGarbageCollection();
- 
- }
-    @Override
-    protected void finalize() {
-     LOGGER.info("Object; " + this.hashCode() + " has been deleted " + (count++));
-    }
+  static int count = 1;
+  private static final Logger LOGGER = Logger.getLogger(GarbageCollectionConcept.class.getName());
+
+  public static void main(String[] args) throws SecurityException, IOException {
+
+    showGarbageCollection();
+
+  }
+
+  @Override
+  protected void finalize() {
+    LOGGER.info("Object; " + this.hashCode() + " has been deleted " + (count++));
+  }
+
   public static void showGarbageCollection() {
-    for (int i = 1; i<=20; i++) {
+    for (int i = 1; i <= 20; i++) {
       GarbageCollectionConcept gc = new GarbageCollectionConcept();
-     LOGGER.info("Object: " + gc.hashCode() + " has been created " + i);
+      LOGGER.info("Object: " + gc.hashCode() + " has been created " + i);
       gc = null;
     }
     System.gc();
   }
-  }
+}
 
